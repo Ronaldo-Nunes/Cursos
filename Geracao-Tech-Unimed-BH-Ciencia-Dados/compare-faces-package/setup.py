16b271d40f578057ecbf053dd7d2b5de12a37c9b
Ronaldo Nunes <projetoftnunes@gmail.com>
2022-10-27 14:36:51 -0300

    Primeiro commit

---
+++ Geracao-Tech-Unimed-BH-Ciencia-Dados/compare-faces-package/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="compare_faces",
+    version="0.0.1",
+    author="Ronaldo Nunes",
+    author_email="projetoftnunes@gmail.com",
+    description="Pacote para comparação de duas faces",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
+    
